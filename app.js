5339c8e18a972b482454c6272750e2377369ca5f
Arsalan707 <96569192+Arsalan707@users.noreply.github.com>
2023-02-15 16:25:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//THIS IS BY USING NODE
 const http = require('http');
 const port = 3000;
 
@@ -11,4 +12,17 @@ server.listen(port, (err)=>{
     }else{
         console.log(`server is listening on port ${port}`)
     }
+})
+
+
+// THIS IS BY USING EXPRESS
+let express = require("express");
+let app = express();
+app.listen(3000, () => {
+ console.log("Server is running on port 3000");
+});
+
+
+app.get('/user',(req,res,next)=>{
+    res.status(200).json(message:"we using get request form /user api and status code is defined 200")
 })
